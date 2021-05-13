e27d355a974f9099ac4deb8385d874d4f65219cd
schw240 <schw240@gmail.com>
2021-05-13 23:14:34 +0900

    디렉토리 정리

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const db = require('../../config/db')
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  const sql3 = 'SELECT * FROM student_tb4'
+  return new Promise(async resolve => {
+    db.query(sql3, function (err, result) {
+      if (err) throw err
+      console.log("Table altered")
+      res.status(200).json(result[0].name)
+      return resolve()
+    })
+  })
 }
