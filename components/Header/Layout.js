a1e2a5151b1818af465d10b68d30b8dcc95b308c
schw240 <schw240@gmail.com>
2021-06-10 16:09:57 +0900

    메인페이지 반응형 + Layout Hamburger menu

--- components/Header/Layout.js
+++ components/Header/Layout.js
@@ -1,14 +1,18 @@
-import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
+import { useState } from 'react';
+
 
 const Layout = ({ children }) => {
+    const [showLinks, setShowLinks] = useState(true);
+
     return (
       <>
         <div className="header">
           <div className="container">
-            <div className="container-left clearfix">
+            <div className="container-left">
+
               <div className="logo">
                 <Link href="/"><a><Image
                       src="/images/logo2.png"
@@ -17,68 +21,71 @@ const Layout = ({ children }) => {
                       height={60}
                 /></a></Link>
               </div>
-              <div className="menu clearfix">
-                <div className="menu-item"><Link href="/about"><a>소개</a></Link></div>
-                <div className="menu-item"><Link href="/project"><a>프로젝트</a></Link></div>
-                <div className="menu-item"><Link href="https://blog.naver.com/schw240"><a>블로그</a></Link></div>
-                <div className="menu-item"><Link href="https://github.com/schw240"><a>깃허브</a></Link></div>
-              </div>
+
+              <ul id={showLinks ? "hidden" : ""} className="menu">
+                <li className="menu-item"><Link href="/about"><a>소개</a></Link></li>
+                <li className="menu-item"><Link href="/project"><a>프로젝트</a></Link></li>
+                <li className="menu-item"><Link href="https://blog.naver.com/schw240"><a>블로그</a></Link></li>
+                <li className="menu-item"><Link href="https://github.com/schw240"><a>깃허브</a></Link></li>
+              </ul>
             </div>
+            
+            {/* 토글버튼 */}
+            <div id="toggle-btn" onClick={() => setShowLinks(!showLinks)}>Header Menu Toggle Button</div>
+
           </div>
         </div>
-        {children}
-        <footer className="footerContainer">
-          <div className="footerContents">
-            <span>김한주 
-              <p>|</p> 
-              <Link href="/policy">
-                <a>개인정보 처리방침</a>
-              </Link>  
-            </span>
-            {/* 링크 */}
-            <ul className="footerLinks">
-              <li className="mail">
-                <Link href="/"><a><Image
-                        src="/images/mail.png"
-                        alt="mail"
-                        width={20}
-                        height={20}
-                /></a></Link>
-              </li>
-              <li className="kakao">
-                <Link href="/"><a><Image
-                        src="/images/kakao.png"
-                        alt="kakao"
-                        width={20}
-                        height={20}
-                /></a></Link>
-              </li>
-              <li className="sns">
-                <Link href="/"><a><Image
-                        src="/images/git.png"
-                        alt="github"
-                        width={20}
-                        height={20}
-                /></a></Link>
-              </li>
-            </ul>
-          </div>
-          
-          <div className="info">
-            <span>
-              김한주 
-              <p>|</p> 
-              이메일 
-              <p>|</p> 
-              scwh240@naver.com 
-              <p>|</p> 
-              카카오톡 schw240 
-              <p>|</p> 
-              깃허브 schw240
-            </span>
-            <div>Copyright ⓒ 2021 KHJ All Rights Reserved.</div>
-          </div>
-        </footer>
+
+        <div id='overlay'>
+          {children}
+
+          <footer className="footerContainer">
+            <div className="footerContents">
+              <span>김한주</span>
+              {/* 링크 */}
+              <ul className="footerLinks">
+                <li className="mail">
+                  <Link href="/"><a><Image
+                          src="/images/mail.png"
+                          alt="mail"
+                          width={20}
+                          height={20}
+                  /></a></Link>
+                </li>
+                <li className="kakao">
+                  <Link href="/"><a><Image
+                          src="/images/kakao.png"
+                          alt="kakao"
+                          width={20}
+                          height={20}
+                  /></a></Link>
+                </li>
+                <li className="sns">
+                  <Link href="/"><a><Image
+                          src="/images/git.png"
+                          alt="github"
+                          width={20}
+                          height={20}
+                  /></a></Link>
+                </li>
+              </ul>
+            </div>
+            
+            <div className="info">
+              <span>
+                <p>김한주 </p> 
+                <p> 이메일 </p> 
+                <p>|</p> 
+                <p> scwh240@naver.com </p>
+                <p>|</p> 
+                <p>카카오톡 schw240 </p>
+                <p>|</p> 
+                <p> 깃허브 schw240 </p>
+              </span>
+              <div>Copyright ⓒ 2021 KHJ All Rights Reserved.</div>
+            </div>
+          </footer>
+        </div>
       </>
     );
   };
