8894e6d72e9931e27e3784d88adea88af6208a40
schw240 <schw240@gmail.com>
2021-07-25 19:50:58 +0900

    블로그 수정

--- components/blog/detail.js
+++ components/blog/detail.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import Link from 'next/link'
-import axios from 'axios'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import styles from '../../styles/Post.module.css'
 
 const BlogDetail = (props) => {
   console.log('----------------------------------')
-  const content = props.detail.content.replace(/\n/gi, '  \n')
+  const { content } = props.detail
+
+  const components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '')
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={tomorrow}
+          language={match[1]}
+          PreTag="div"
+          children={String(children).replace(/\n$/, '')}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props} />
+      )
+    },
+  }
 
   const Heading1 = ({ node, ...props }) => (
     <h1
@@ -45,6 +63,12 @@ const BlogDetail = (props) => {
       {...props}
     />
   )
+  const LinkStyle = ({ node, ...props }) =>
+    props.href.match(/^(http?)?\/\//) ? (
+      <a href={props.href}>{props.children}</a>
+    ) : (
+      <Link to={props.href}>{props.children}</Link>
+    )
 
   return (
     <>
@@ -57,15 +81,7 @@ const BlogDetail = (props) => {
         {/* Content */}
         <section className={styles.content}>
           <div className={styles.contentDetail}>
-            <ReactMarkdown
-              components={{
-                blockquote: BlockQuoteStyle,
-                h1: Heading1,
-                code: CodeBlock,
-                img: imageStyle,
-              }}
-              children={content}
-            />
+            <ReactMarkdown components={components} children={content} />
           </div>
         </section>
       </div>
--- components/layout/header.js
+++ components/layout/header.js
@@ -1,11 +1,9 @@
 import Link from 'next/link'
-import PropTypes from 'prop-types'
 import axios from 'axios'
 import Image from 'next/image'
 import { useState } from 'react'
 
 const Header = () => {
-  // console.log(cate)
   const [showLinks, setShowLinks] = useState(false)
 
   return (
@@ -28,40 +26,27 @@ const Header = () => {
           <ul id={showLinks ? 'hidden' : ''} className="menu">
             <li className="menu-item">
               <Link href="/about">
-                <a>소개</a>
+                <a>About</a>
               </Link>
             </li>
             <li className="menu-item">
               <Link href="/project">
-                <a>프로젝트</a>
+                <a>Projects</a>
               </Link>
             </li>
             <li className="menu-item">
               <Link href="/blog">
-                <a>블로그</a>
+                <a>Blog</a>
               </Link>
-              {/* <ul>
-                <li>파이썬</li>
-                <li>JS</li>
-                <li>DataStructure</li>
-              </ul> */}
             </li>
             <li className="menu-item">
               <Link href="https://github.com/schw240">
-                <a>깃허브</a>
+                <a>GitHub</a>
               </Link>
             </li>
           </ul>
         </div>
-        <div>
-          {/* {cate.map((lst) => (
-            <div key={lst.id}>
-              <Link href="/">
-                <a>{lst.name}</a>
-              </Link>
-            </div>
-          ))} */}
-        </div>
+        <div />
 
         {/* 토글버튼 */}
         <div id="toggle-btn" onClick={() => setShowLinks(!showLinks)}>
@@ -71,9 +56,5 @@ const Header = () => {
     </div>
   )
 }
-
-// Header.propTypes = {
-//   cate: PropTypes.node.isRequired,
-// }
 
 export default Header
