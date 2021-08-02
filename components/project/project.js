2f305b6ad5a1d07deac94d54e492635b820677f9
schw240 <schw240@gmail.com>
2021-08-02 09:20:00 +0900

    프로젝트 수정(ing)

---
+++ components/project/project.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import projectText from '../../public/texts/project'
+import styles from '../../styles/Project.module.css'
+
+const Project = ({}) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.sectionTitle}>
+          <span>
+            <Image src="/projects.png" width={180} height={45} />
+          </span>
+        </div>
+        <section className={styles.projectSection}>
+          {projectText.project.map((el) => (
+            <div className={styles.eachProject} key={el.name}>
+              <div className={styles.imgThumb}>
+                <Image src={el.img} width={450} height={450} />
+              </div>
+              <div className={styles.projectInfo}>
+                <h1>{el.title}</h1>
+                <h4>{el.info}</h4>
+                <h4>({el.date})</h4>
+                <p>{el.desc}</p>
+                <div className={styles.toLink}>
+                  <span>
+                    <Link href={el.link}>
+                      <a>View Demo</a>
+                    </Link>
+                  </span>
+                  {el.code !== '' ? (
+                    <span>
+                      <Link href={el.code}>
+                        <a>Source Code</a>
+                      </Link>
+                    </span>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default Project
---
+++ pages/project.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Head from 'next/head'
+import Project2 from '../components/project/project'
+
+const Project = ({}) => {
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png?v=2" />
+
+        <title>sunhodev - projects</title>
+        <meta name="author" content="sunho" />
+        <meta name="description" content="sunhodev - 프로젝트" />
+        <meta
+          name="keywords"
+          content="web, javascript, os, sunhomusic, sunhodev, graphql, aws, linux, vue, nuxt, node, frontend, backend, 백엔드, 프론트엔드, 데브옵스, 기술블로그"
+        />
+        <meta property="og:title" content="💻 sunhodev" />
+        <meta property="og:description" content="sunhodev - 프로젝트" />
+        <meta property="og:url" content="https://sunhodev.com/project" />
+        <meta property="og:site_name" content="sunhodev" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://sunhodev.com/meta-sunhodev.png"
+        />
+      </Head>
+      <Project2 />
+    </>
+  )
+}
+
+export default Project
